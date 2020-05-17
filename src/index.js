a42e256fea82cb72eafe33215a0228faf82ba234
coseo12 <momo_12@naver.com>
2020-05-17 12:54:44 +0900

    불필요 라인 삭제

--- src/index.js
+++ src/index.js
@@ -11,10 +11,10 @@ const currying = (f, len = f.length) => {
 //     bs.length ? f(a, ...bs) : (...bs) => f(a, ...bs);
 //   };
 // };
-const filter = currying(function*(f, iter) {
+const filter = currying(function* (f, iter) {
   for (const i of iter) if (f(i)) yield i;
 });
-const map = currying(function*(f, iter) {
+const map = currying(function* (f, iter) {
   for (const i of iter) yield f(i);
 });
 const take = currying((len, iter) => {
@@ -37,13 +37,8 @@ const go = (ac, ...fs) => reduce((acc, f) => f(acc), ac, fs);
 console.log(
   go(
     [0, 1, 2, 3, 4, 5],
-    filter(a => a > 2),
-    map(a => a + 2),
-    take(3),
-  ),
+    filter((a) => a > 2),
+    map((a) => a + 2),
+    take(3)
+  )
 );
-
-// console.log(reduce((a, b) => a + b)([0, 1, 2, 3, 4, 5])(undefined));
-
-// const test = reduce((a, b) => a + b, [0, 1, 2, 3, 4, 5]);
-// console.log(test());
