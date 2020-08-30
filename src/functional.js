e84c98202670ad67417f1a98211d339d48a5ea12
coseo12 <momo_12@naver.com>
2020-08-30 15:47:30 +0900

    dd

--- src/functional.js
+++ src/functional.js
@@ -1,79 +1,87 @@
-//* currying function
-const currying = (f, len = f.length) => {
-  return (function recur(prevArgs) {
-    return (...curArgs) => {
-      const args = [...prevArgs, ...curArgs];
-      return args.length >= len ? f(...args) : recur(args);
-    };
-  })([]);
-};
-//* pipe function
-const pipe = (f1, ...fs) => (...iter) =>
-  L.reduce((acc, f) => f(acc), f1(...iter), fs);
-//* go function
-const go = (iter, ...fs) => pipe(...fs)(iter);
-// const go = (ac, ...fs) => reduce((acc, f) => f(acc), ac, fs);
+export const log = console.log;
+export const curry = f => (a, ..._) =>
+  _.length ? f(a, ..._) : (..._) => f(a, ..._);
+export const go = (...args) => reduce((a, f) => f(a), args);
+export const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs);
 
-//* compose function
-const compose = (...fns) => args => fns.reduceRight((arg, fn) => fn(arg), args);
-const _push = (val, arr = []) => (arr.push(val), arr);
-
-//* Immediately functional javascript
-const I = {
-  //* map function
-  // map(fn, iter) {
-  //   const res = [];
-  //   for (const item of iter) {
-  //     res.push(fn(item));
-  //   }
-  //   return res;
-  // },
-  map: currying((f, iter) =>
-    L.reduce((acc, cur) => _push(f(cur), acc), [], iter)
-  ),
-  //* filter function
-  // filter(fn, iter) {
-  //   const res = [];
-  //   for (const item of iter) fn(item) && res.push(item);
-  //   return res;
-  // },
-  filter: currying((f, iter) =>
-    L.reduce((acc, cur) => (f(cur) ? _push(cur, acc) : acc), [], iter)
-  ),
-  //* reduce function
-  reduce(fn, acc, coll) {
-    if (coll === undefined) {
-      coll = acc[Symbol.iterator]();
-      acc = coll.next().value;
-    }
-    for (const item of coll) acc = fn(acc, item);
-    return acc;
-  },
-};
-//* Lazy functional javascript
-const L = {
-  filter: currying(function* (f, iter) {
-    for (const i of iter) if (f(i)) yield i;
-  }),
-  map: currying(function* (f, iter) {
-    for (const i of iter) yield f(i);
-  }),
-  take: currying((len, iter) => {
-    const res = [];
-    for (const i of iter) {
+export const I = {
+  range: l => {
+    let i = -1;
+    let res = [];
+    while (++i < l) {
       res.push(i);
-      if (res.length == len) break;
+    }
+    return res;
+  },
+  map: curry((f, iter) => {
+    let res = [];
+    iter = iter[Symbol.iterator]();
+    let cur;
+    while (!(cur = iter.next()).done) {
+      const a = cur.value;
+      res.push(f(a));
     }
     return res;
   }),
-  reduce: currying((f, acc, iter) => {
-    if (iter === undefined) {
+  filter: curry((f, iter) => {
+    let res = [];
+    iter = iter[Symbol.iterator]();
+    let cur;
+    while (!(cur = iter.next()).done) {
+      const a = cur.value;
+      if (f(a)) res.push(a);
+    }
+    return res;
+  }),
+  take: curry((l, iter) => {
+    let res = [];
+    iter = iter[Symbol.iterator]();
+    let cur;
+    while (!(cur = iter.next()).done) {
+      const a = cur.value;
+      res.push(a);
+      if (res.length == l) return res;
+    }
+    return res;
+  }),
+  reduce: curry((f, acc, iter) => {
+    if (!iter) {
       iter = acc[Symbol.iterator]();
       acc = iter.next().value;
+    } else {
+      iter = iter[Symbol.iterator]();
     }
-    for (const i of iter) acc = f(acc, i);
+    let cur;
+    while (!(cur = iter.next()).done) {
+      const a = cur.value;
+      acc = f(acc, a);
+    }
     return acc;
   }),
 };
-
-module.exports = { currying, compose, pipe, go, I, L };
+export const L = {
+  range: function* (l) {
+    let i = -1;
+    while (++i < l) {
+      yield i;
+    }
+  },
+  map: curry(function* (f, iter) {
+    iter = iter[Symbol.iterator]();
+    let cur;
+    while (!(cur = iter.next()).done) {
+      const a = cur.value;
+      yield f(a);
+    }
+  }),
+  filter: curry(function* (f, iter) {
+    iter = iter[Symbol.iterator]();
+    let cur;
+    while (!(cur = iter.next()).done) {
+      const a = cur.value;
+      if (f(a)) {
+        yield a;
+      }
+    }
+  }),
+};
